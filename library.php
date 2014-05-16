933e1c76e1f71ef99e9ab879c740bd78b3b111b0
Ben Keen <ben.keen@gmail.com>
2014-05-15 20:53:15 -0700

    1.0.2

--- library.php
+++ library.php
@@ -18,7 +18,7 @@ function hd_get_rand_nums()
 
   $numbers = array();
   while ($row = mysql_fetch_assoc($query))
-  	$numbers[] = $row["random_number"];
+    $numbers[] = $row["random_number"];
 
   return $numbers;
 }
@@ -33,21 +33,21 @@ function hello_database__install($module_id)
   global $g_table_prefix;
 
   mysql_query("
-		CREATE TABLE {$g_table_prefix}module_hello_database (
-		  row_id smallint(5) unsigned NOT NULL auto_increment PRIMARY KEY,
-		  random_number smallint(5)
-		  ) ENGINE=InnoDB
-		");
+    CREATE TABLE {$g_table_prefix}module_hello_database (
+      row_id smallint(5) unsigned NOT NULL auto_increment PRIMARY KEY,
+      random_number smallint(5)
+      ) TYPE=MyISAM
+    ");
 
-	// populate the database with some random numbers
+  // populate the database with some random numbers
   for ($i=1; $i<=10; $i++)
   {
-  	$random_number = mt_rand(1, 1000);
+    $random_number = mt_rand(1, 1000);
 
-  	mysql_query("
-			INSERT INTO {$g_table_prefix}module_hello_database (random_number)
-			VALUES ($random_number)
-  	");
+    mysql_query("
+      INSERT INTO {$g_table_prefix}module_hello_database (random_number)
+      VALUES ($random_number)
+    ");
   }
 
   return array(true, "");
