df0c0bd936094f23561f99df0caaa18b1cc04050
Ben Keen <ben.keen@gmail.com>
2014-05-15 20:51:42 -0700

    1.0.1

--- module.php
+++ module.php
@@ -7,12 +7,12 @@
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.0.0";
-$MODULE["date"]            = "2008-12-29";
+$MODULE["version"]         = "1.0.1";
+$MODULE["date"]            = "2009-01-14";
 $MODULE["origin_language"] = "en_us";
 $MODULE["supports_ft_versions"] = "2.0.0";
 
 $MODULE["nav"] = array(
-  "module_name" => array('index.php', false),
+  "module_name" => array("index.php", false),
   "word_settings" => array("settings.php", false)
     );
